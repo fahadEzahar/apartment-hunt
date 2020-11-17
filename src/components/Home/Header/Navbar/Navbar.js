964e3ba251c64ec81377a01529848e4b52bafd2e
fahadEzahar <ezaharfahad01@gmail.com>
2020-11-17 13:00:35 +0600

    navbar collapse button updated

--- src/components/Home/Header/Navbar/Navbar.js
+++ src/components/Home/Header/Navbar/Navbar.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../../logos/Logo.png'
 
 const Navbar = () => {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
     return (
         <div class="container pb-3">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <a class="navbar-brand" href="/home">
                     <img class="logo" src={logo} alt="" />
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+                <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item active m-2">
                             <a class="nav-link text-dark font-weight-bold" href="/home">Home <span class="sr-only">(current)</span></a>
